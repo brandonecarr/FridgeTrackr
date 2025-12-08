52be53d83346db6a0a759308e16cea11763fa994
Name <brandonecarr@gmail.com>
2025-12-08 20:11:59 +0000

    fix

--- metro.config.js
+++ metro.config.js
@@ -1,12 +1,9 @@
+// Learn more https://docs.expo.io/guides/customizing-metro
 const { getDefaultConfig } = require("expo/metro-config");
 const { withNativeWind } = require("nativewind/metro");
 
+/** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname);
-
-// Exclude problematic directories from watchman
-config.watchFolders = config.watchFolders || [];
-config.resolver = config.resolver || {};
-config.resolver.blacklistRE = /node_modules\/(@emnapi\/runtime\/dist|@tybys\/wasm-util\/lib\/cjs\/wasi)/;
 
 module.exports = withNativeWind(config, { input: "./global.css" });
 
