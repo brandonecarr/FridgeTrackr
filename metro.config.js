a94011e249b6d833dbeb55a8e4d44e3535ff91c9
Name <brandonecarr@gmail.com>
2025-12-09 01:09:50 +0000

    hu

--- metro.config.js
+++ metro.config.js
@@ -1,12 +1,11 @@
 // Learn more https://docs.expo.io/guides/customizing-metro
 const { getDefaultConfig } = require("expo/metro-config");
-const { withNativeWind } = require("nativewind/metro");
 
 /** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname);
 
-module.exports = withNativeWind(config, {
-  input: "./global.css",
-  configPath: "./tailwind.config.js",
-});
+// NativeWind CSS support
+config.resolver.sourceExts.push("css");
+
+module.exports = config;
 
